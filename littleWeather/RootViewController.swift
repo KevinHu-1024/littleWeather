1ba6578045d65d37c81181acad2f3a8b738a566b
darknighten <hi-bai-du@163.com>
2015-03-31 07:01:46 +0800

    test

--- littleWeather/RootViewController.swift
+++ littleWeather/RootViewController.swift
@@ -17,7 +17,6 @@ class RootViewController: UIViewController, UIPageViewControllerDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Configure the page view controller and add it as a child view controller.
-        //
         self.pageViewController = UIPageViewController(transitionStyle: .PageCurl, navigationOrientation: .Horizontal, options: nil)
         self.pageViewController!.delegate = self
 
