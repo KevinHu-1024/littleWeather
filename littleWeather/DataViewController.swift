bc3d9092022dd714e8eec4f0bdb6503263b3cb48
darknighten <hi-bai-du@163.com>
2015-03-31 06:57:12 +0800

    littleWeather

---
+++ littleWeather/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  littleWeather
+//
+//  Created by Mac on 15/3/31.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
---
+++ littleWeather/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  littleWeather
+//
+//  Created by Mac on 15/3/31.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
