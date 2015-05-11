eca31f02ed288b312d4770f7db750ddd3de10227
darknighten <hi-bai-du@163.com>
2015-05-11 22:38:25 +0800

    Modify AutoLayout

--- littleWeather/MainViewController.swift
+++ littleWeather/MainViewController.swift
@@ -48,30 +48,6 @@ class MainViewController: UIViewController , UITableViewDelegate, UITableViewDat
         tbv?.reloadData()
     }
     
-//    func setCellColorBy(weather:String) ->UIColor{
-//        //对String提取最后一个字
-//        //对最后一个字进行swich，每种状况设置一个颜色，然后break，外面返回cellColor
-//        var cellColor = UIColor.defaultColor()
-//        if weather.isEmpty {
-//            println("cellIsEmpty")
-//        }else{
-//        switch weather[weather.endIndex.predecessor()] {
-//        case "晴":
-//            cellColor = UIColor.clearColor()
-//        case "云":
-//            cellColor = UIColor.cloudColor()
-//        case "霾":
-//            cellColor = UIColor.maiColor()
-//        case "雨":
-//            cellColor = UIColor.rainColor()
-//        default:
-//            cellColor = UIColor.defaultColor()
-//        }
-//        }
-//        return cellColor
-//    }
-    
-
     // MARK: - TableViewDataSource
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -92,8 +68,6 @@ class MainViewController: UIViewController , UITableViewDelegate, UITableViewDat
         switch indexPath.row {
         case 0:
             var weatherCell = tableView.dequeueReusableCellWithIdentifier("WeatherCell", forIndexPath: indexPath) as MainTableViewCell
-//            weatherCell.backgroundColor = UIColor(red: 20/255, green: 200/255, blue: 180/255, alpha: 1)
-//            weatherCell.backgroundColor = setCellColorBy(weatherService.weatherInfoNow.weather)
             weatherCell.setBackgroundColorBy(weatherService.weatherInfoNow.weather)
             weatherCell.city?.text = weatherService.weatherInfoNow.city
             weatherCell.temp?.text = weatherService.weatherInfoNow.temp
@@ -103,8 +77,6 @@ class MainViewController: UIViewController , UITableViewDelegate, UITableViewDat
             
         case 1:
             var weatherCell = tableView.dequeueReusableCellWithIdentifier("WeatherCell", forIndexPath: indexPath) as MainTableViewCell
-//            weatherCell.backgroundColor = UIColor(red: 20/255, green: 200/255, blue: 180/255, alpha: 1)
-//            weatherCell.backgroundColor = setCellColorBy(weatherService.weatherInfo.weather)
             weatherCell.setBackgroundColorBy(weatherService.weatherInfo.weather)
             weatherCell.city?.text = weatherService.weatherInfo.city
             weatherCell.temp?.text = weatherService.weatherInfo.temp
@@ -114,8 +86,6 @@ class MainViewController: UIViewController , UITableViewDelegate, UITableViewDat
             
         case 2:
             var weatherCell = tableView.dequeueReusableCellWithIdentifier("WeatherCell", forIndexPath: indexPath) as MainTableViewCell
-//            weatherCell.backgroundColor = UIColor(red: 20/255, green: 200/255, blue: 180/255, alpha: 1)
-//            weatherCell.backgroundColor = setCellColorBy(weatherService.weatherInfo2.weather)
             weatherCell.setBackgroundColorBy(weatherService.weatherInfo2.weather)
             weatherCell.city?.text = weatherService.weatherInfo2.city
             weatherCell.temp?.text = weatherService.weatherInfo2.temp
@@ -138,9 +108,9 @@ class MainViewController: UIViewController , UITableViewDelegate, UITableViewDat
     func tableView( tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         
         var rect_screen = UIScreen.mainScreen().bounds.size
-        println("rect_screen = \(rect_screen.height)")
+//        println("rect_screen = \(rect_screen.height)")
         var cellHeight = (rect_screen.height - 58)/3
-        println("cellHeight = \(cellHeight)")
+//        println("cellHeight = \(cellHeight)")
         
         return cellHeight
         
